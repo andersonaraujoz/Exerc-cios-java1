1542b10c3f4551b37b0171df4a5e565f12e8525a
Araujoz <souza.ajsz@gmail.com>
2022-06-05 08:54:45 -0300

    Mais Exercícios

---
+++ JavaModulo1/src/exercicio7.java
@@ -0,0 +1,14 @@
+
+public class exercicio7 {
+
+	public static void main(String[] args) {
+	
+		int soma = 0;
+		
+	for(int i = 1; i <= 100; i++) {
+		System.out.println(i);
+		soma = soma + i;
+	}
+	
+	}
+}
---
+++ JavaModulo1/src/exerecicio6.java
@@ -0,0 +1,12 @@
+
+public class exerecicio6 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 100; i <= 250; i++) {
+		System.out.println(i);
+
+	}
+	}
+
+}
